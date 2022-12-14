7786c8314d8b275ec755c866379d164eb1552e35
green spot <in.green.spot@gmail.com>
2022-12-14 19:34:17 +0900

    fix

--- src/Pachira/Pachira.php
+++ src/Pachira/Pachira.php
@@ -8,12 +8,9 @@ require_once __DIR__ . "/NextRoute.php";
 require_once __DIR__ . "/View.php";
 
 class Pachira {
-  public static function init($options){
+  public static function run($options=[]){
+    $path = el($options, "path", el($_SERVER, "PATH_INFO", "/"));
     Pachira\View::view_dir(el($options, "view_dir", null));
-  }
-
-  public static function run($path=null){
-    $path = $path ?: el($_SERVER, "PATH_INFO", "/");
     Pachira\Router::getInstance()->route($path);
   }
 }
